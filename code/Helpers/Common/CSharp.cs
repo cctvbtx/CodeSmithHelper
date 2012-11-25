0f9b1155d580d065d35e10641f44cade8a513673
Steven Macinnis <smacinn@yahoo.com>
2012-11-25 10:36:37 -0500

    Initial Commit

---
+++ code/Helpers/Common/CSharp.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SchemaExplorer;
+using CodeSmith.Core.Extensions;
+
+namespace Dev.CSmith.Common
+{
+    public static class CSharp
+    {
+        public static string GetCSharpVariableType(ColumnSchema column)
+        {
+            if (column.Name.EndsWith("TypeCode")) return column.Name;
+
+            switch (column.DataType)
+            {
+                case DbType.AnsiString: return "string";
+                case DbType.AnsiStringFixedLength: return "string";
+                case DbType.Binary: return "byte []";
+                case DbType.Boolean: return "bool";
+                case DbType.Byte: return "byte";
+                case DbType.Currency: return "decimal";
+                case DbType.Date: return "DateTime";
+                case DbType.DateTime: return "DateTime";
+                case DbType.Decimal: return "decimal";
+                case DbType.Double: return "double";
+                case DbType.Guid: return "Guid";
+                case DbType.Int16: return "short";
+                case DbType.Int32: return "int";
+                case DbType.Int64: return "long";
+                case DbType.Object: return "object";
+                case DbType.SByte: return "sbyte";
+                case DbType.Single: return "float";
+                case DbType.String: return "string";
+                case DbType.StringFixedLength: return "string";
+                case DbType.Time: return "TimeSpan";
+                case DbType.UInt16: return "ushort";
+                case DbType.UInt32: return "uint";
+                case DbType.UInt64: return "ulong";
+                case DbType.VarNumeric: return "decimal";
+                default:
+                    {
+                        return "__UNKNOWN__" + column.NativeType;
+                    }
+            }
+        }
+
+        public static string GetCSharpConvert(ColumnSchema column, string placeholder)
+        {
+            if (column.Name.EndsWith("TypeCode")) return column.Name;
+
+            switch (column.DataType)
+            {
+                case DbType.AnsiString: return placeholder;
+                case DbType.AnsiStringFixedLength: return placeholder;
+                //case DbType.Binary: return "byte []";
+                case DbType.Boolean: return "Convert.ToBoolean(" + placeholder + ");";
+                case DbType.Byte: return "Convert.ToByte(" + placeholder + ")";
+                case DbType.Currency: return "Convert.ToDecimal(" + placeholder + ")";
+                case DbType.Date: return placeholder;
+                case DbType.DateTime: return placeholder;
+                case DbType.Decimal: return "Convert.ToDecimal(" + placeholder + ")";
+                case DbType.Double: return "Convert.ToDouble(" + placeholder + ")";
+                case DbType.Guid: return placeholder;
+                case DbType.Int16: return "Convert.ToInt16(" + placeholder + ")";
+                case DbType.Int32: return "Convert.ToInt32(" + placeholder + ")";
+                case DbType.Int64: return "Convert.ToInt64(" + placeholder + ")";
+                case DbType.Object: return placeholder;
+                case DbType.SByte: return "Convert.ToSByte(" + placeholder + ")";
+                case DbType.Single: return "Convert.ToFloat(" + placeholder + ")";
+                case DbType.String: return placeholder;
+                case DbType.StringFixedLength: return placeholder;
+                case DbType.Time: return "TimeSpan";
+                case DbType.UInt16: return "Convert.ToUInt16(" + placeholder + ")";
+                case DbType.UInt32: return "Convert.ToUInt32(" + placeholder + ")";
+                case DbType.UInt64: return "Convert.ToUInt64(" + placeholder + ")";
+                case DbType.VarNumeric: return "Convert.ToDecimal(" + placeholder + ")";
+                default:
+                    {
+                        return "__UNKNOWN__" + column.NativeType;
+                    }
+            }
+        }
+
+    }
+}
---
+++ code/Helpers/Utilities/DB.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SchemaExplorer;
+using CodeSmith.Core.Extensions;
+
+namespace Dev.CSmith.Utilities
+{
+    public static class DB
+    {
+        /// <summary>
+        /// Helper for class generation from tables, converts a column name into a Property Name
+        /// </summary>
+        /// <param name="column"></param>
+        /// <returns>string</returns>
+        public static string GetPropertyNameFromColumn(ColumnSchema column)
+        {
+            return GetPropertyNameFromColumn(column, "", "");
+        }
+
+        /// <summary>
+        /// Helper for class generation from tables, converts a column name into a Property Name
+        /// </summary>
+        /// <param name="column"> Column to be converted</param>
+        /// <param name="prefix">Add characters to column</param>
+        /// <param name="strip">remove characters from column</param>
+        /// <returns>string</returns>
+        public static string GetPropertyNameFromColumn(ColumnSchema column, string prefix, string strip)
+        {
+            return prefix + column.Name.Replace(strip, "").ToCSharpIdentifier().ToPascalCase();
+        }
+
+        /// <summary>
+        /// Helper to generate class name from a table
+        /// </summary>
+        /// <param name="table">table to be converted</param>
+        /// <returns>string /returns>
+        public static string GetClassNameFromTable(TableSchema table)
+        {
+            return GetClassNameFromTable(table, "", "");
+        }
+
+        /// <summary>
+        /// Helper to generate class name from a table
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="prefix"></param>
+        /// <param name="strip"></param>
+        /// <returns>string</returns>
+        public static string GetClassNameFromTable(TableSchema table, string prefix, string strip)
+        {
+            return prefix + table.Name.Replace(strip, "").ToCSharpIdentifier().ToPascalCase();
+        }
+
+        /// <summary>
+        /// Is the current column an Identity column
+        /// </summary>
+        /// <param name="column">table column to be checked</param>
+        /// <returns>True/False</returns>
+        public static bool IsIdentityColumn(ColumnSchema column)
+        {
+            return (bool)column.ExtendedProperties["CS_IsIdentity"].Value;
+        }
+
+
+        // todo
+        /*
+        public string GetPrimaryKeyParameters(TableSchema table, bool includeTypes)
+        {
+            string parameters = "";
+
+            if (table.PrimaryKey != null)
+            {
+                for (int i = 0; i < table.PrimaryKey.MemberColumns.Count; i++)
+                {
+                    if (parameters.Length == 0)
+                    {
+                        if (includeTypes)
+                        {
+                            parameters = parameters + GetCSharpVariableType(table.PrimaryKey.MemberColumns[i]) + " ";
+                        }
+                        parameters = parameters + table.PrimaryKey.MemberColumns[i].Name.ToPascalCase();
+                    }
+                    else
+                    {
+                        parameters = parameters + ", ";
+
+                        if (includeTypes)
+                        {
+                            parameters = parameters + GetCSharpVariableType(table.PrimaryKey.MemberColumns[i]) + " ";
+                        }
+                        parameters = parameters + table.PrimaryKey.MemberColumns[i].Name.ToPascalCase();
+                    }
+                }
+            }
+            else
+            {
+                throw new ApplicationException("This template will only work on tables with a primary key.");
+            }
+            return parameters;
+        }
+
+        public string DnnSqlModel(SchemaExplorer.TableSchema SourceTable)
+        {
+            return "IDnn" + SourceTable.Name.Replace("NetTv_", "") + "Sql";
+        }
+        public string DnnDataModel(SchemaExplorer.TableSchema SourceTable)
+        {
+            return "IDnn" + SourceTable.Name.Replace("NetTv_", "").ToCSharpIdentifier().ToPascalCase();
+        }
+        public string DnnTableModel(SchemaExplorer.TableSchema SourceTable)
+        {
+            return "IDnn" + SourceTable.Name.Replace("NetTv_", "").ToCSharpIdentifier().ToPascalCase() + "Table";
+        }
+        public string ITableModel(SchemaExplorer.TableSchema SourceTable)
+        {
+            return "I" + SourceTable.Name.Replace("NetTv_", "").ToCSharpIdentifier().ToPascalCase() + "Table";
+        }
+        public string DataModel(SchemaExplorer.TableSchema SourceTable)
+        {
+            return "I" + SourceTable.Name.Replace("NetTv_", "").ToCSharpIdentifier().ToPascalCase();
+        }
+        public string IDnnDal(SchemaExplorer.TableSchema SourceTable)
+        {
+            return "I" + SourceTable.Name.Replace("NetTv_", "");
+        }
+        public string ISql(SchemaExplorer.TableSchema SourceTable)
+        {
+            return "I" + SourceTable.Name.Replace("NetTv_", "") + "Sql";
+        }
+
+        public string IRepository(SchemaExplorer.TableSchema SourceTable)
+        {
+            return "I" + SourceTable.Name.Replace("NetTv_", "") + "Repository";
+        }
+
+        public string IDal(SchemaExplorer.TableSchema SourceTable)
+        {
+            return "I" + SourceTable.Name.Replace("NetTv_", "") + "Dal";
+        }
+
+        public string CreateColumnList(TableSchema table)
+        {
+            StringBuilder sb = new StringBuilder();
+            int indexer = 1;
+            foreach (ColumnSchema column in table.Columns)
+            {
+                sb.Append("{databaseOwner}[{objectQualifier}" + table.Name + "].[" + column.Name + "]");
+                if (indexer < table.Columns.Count)
+                    sb.Append(",");
+                indexer++;
+            }
+            return sb.ToString();
+        }
+
+        */
+
+    }
+}
